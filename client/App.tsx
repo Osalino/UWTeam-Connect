0098fa140892dca9aefc8486165b61f623007192
osalino <osazemen.odeh@gmail.com>
2026-04-19 19:07:24 +0100

    Added full project files

--- client/App.tsx
+++ client/App.tsx
@@ -375,16 +375,3 @@ export default function App() {
     </QueryClientProvider>
   );
 }
-//
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Index from "./pages/Index";
-//
-// export default function App() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/" element={<Index />} />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
